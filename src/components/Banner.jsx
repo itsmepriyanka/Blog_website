51cc8d1910d4e6f4f1495ceaaf91af90fcb14921
Reshma7707 <gamalreshma26@gmail.com>
2025-03-02 13:24:52 +0545

    banner completed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ import{FaArrowRight} from "react-icons/fa6"
  
 const Banner = () => {
   return (
-    <div className='px-4 py-32 bg-blue-500 mx-auto'>
+    <div className='px-4 py-32 color mx-auto'>
         <div className='text-white text-center'>
             <h1 className='text-5xl lg:text-7xl ledding-snug font-bold mb-5'>Welcome to Our Blog</h1>
             <p className='text-gray-100 lg:w-3/5 mx-auto ' >Start you blog today and join a community of written and readers who are 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className=' py-40 bg-blue-500 mx-auto '>
+    <div className=' py-40 color mx-auto '>
     <div className='text-white text-center'>
         <h1 className='text-5xl lg:text-7xl ledding-snug font-bold mb-5'> About Us Page</h1>
         
